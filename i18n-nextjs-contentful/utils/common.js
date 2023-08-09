ac292310c758adcf0d07a7dc5b06d05b46fb2791
test-denar <54980164+test-denar@users.noreply.github.com>
2023-08-09 10:22:10 +0300

    Update common.js

--- i18n-nextjs-contentful/utils/common.js
+++ i18n-nextjs-contentful/utils/common.js
@@ -2,7 +2,7 @@ export const PAGE_TYPE = 'page';
 export const PAGE_TYPES = [PAGE_TYPE];
 export const SITE_CONFIG_TYPE = 'siteConfig';
 
-export const IS_DEV = process.env.NODE_ENV === 'development';
+export const IS_DEV = process.env.NODE_ENV === 'development' || process.env.IS_DEV;
 
 export function normalizeSlug(slug) {
   return slug.startsWith('/') ? slug : '/' + slug;
